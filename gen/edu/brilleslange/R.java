f4e0acbba3b33c07d9a32324237526fc4b9d914c
selinet <seline@tomt.net>
2011-10-03 14:40:54 +0200

    seline

--- gen/edu/brilleslange/R.java
+++ gen/edu/brilleslange/R.java
@@ -11,8 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int facebook_icon=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int icon=0x7f020000;
     }
     public static final class id {
         public static final int bookALibrarianUsername=0x7f050001;
