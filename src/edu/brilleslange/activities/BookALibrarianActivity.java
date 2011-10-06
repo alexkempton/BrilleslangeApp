2393e74eac7876a17c98a958be10cc797cf07260
Aleksander Rem <alexx87@gmail.com>
2011-10-06 03:46:46 +0200

    Jobbet videre med bestill bibliotekar :)

--- src/edu/brilleslange/activities/BookALibrarianActivity.java
+++ src/edu/brilleslange/activities/BookALibrarianActivity.java
@@ -6,7 +6,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.EditText;
 
 public class BookALibrarianActivity extends Activity {
@@ -15,7 +15,7 @@ public class BookALibrarianActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.bookalibrarian);
-		Button b = (Button)findViewById(R.id.bookbutton);
+		ImageButton b = (ImageButton)findViewById(R.id.bookbutton);
 		
 		
 		b.setOnClickListener(new View.OnClickListener() {
