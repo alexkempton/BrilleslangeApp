df40f423d3617054bf148d0b7cb9029846f5680b
Alexander Kempton <alex@kempton.no>
2011-10-10 18:43:25 +0200

    Sokeskjerm inneholder naa resultater

--- src/edu/brilleslange/activities/BookResultActivity.java
+++ src/edu/brilleslange/activities/BookResultActivity.java
@@ -12,21 +12,26 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class BookResultActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.searchresults);
+		setContentView(R.layout.bookresult);
 		Bundle extras = getIntent().getExtras(); 
-		Record r;
+		Record r = null;
 		if(extras !=null) {
 			r = (Record) getIntent().getSerializableExtra("Record");
-
 		}
 
-
-
+		TextView title = (TextView)findViewById(R.id.bookresulttitletext);
+		TextView author = (TextView)findViewById(R.id.bookresultauthortext);
+		TextView placement = (TextView)findViewById(R.id.bookresultplacementtext);
+		title.setText(r.title);
+		author.setText(r.author);
+		placement.setText(r.placement);
+		
 
 	}
 }
